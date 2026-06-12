8fe91dfbd31b59049b8bc202c8551f0cb38afe1a
vishnu R <157986076+vishnu-R2005@users.noreply.github.com>
2026-06-12 12:54:50 +0530

    Cleared the bug with wishlist end point .

--- backend/apps/products/urls.py
+++ backend/apps/products/urls.py
@@ -5,10 +5,10 @@ from .views import CategoryViewSet, ProductViewSet, ReviewViewSet, WishlistViewS
 
 router = DefaultRouter()
 router.register('categories', CategoryViewSet, basename='category')
-router.register('', ProductViewSet, basename='product')
 router.register('reviews', ReviewViewSet, basename='review')
 router.register('wishlist', WishlistViewSet, basename='wishlist')
 
+router.register('', ProductViewSet, basename='product')
 urlpatterns = [
     path('', include(router.urls)),
 ]
